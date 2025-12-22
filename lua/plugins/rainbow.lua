3fd76bda2a9b36af1a535b4998ba752da7df13bc
edtosoy <edberto.tosoy@gmail.com>
2025-12-22 18:28:52 +0800

    style: precision map VS Code One Dark Pro hex codes to Neovim

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -3,28 +3,31 @@ return {
     "hiphish/rainbow-delimiters.nvim",
     dependencies = "nvim-treesitter/nvim-treesitter",
     config = function()
-      -- This module contains a number of default settings
       local rainbow_delimiters = require("rainbow-delimiters")
 
       vim.g.rainbow_delimiters = {
         strategy = {
           [""] = rainbow_delimiters.strategy["global"],
-          vim = rainbow_delimiters.strategy["global"],
         },
         query = {
           [""] = "rainbow-delimiters",
           lua = "rainbow-blocks",
         },
         highlight = {
+          "RainbowDelimiterOrange", -- VS Code Level 1 (#d19a66)
+          "RainbowDelimiterPurple", -- VS Code Level 2 (#c678dd)
+          "RainbowDelimiterCyan",   -- VS Code Level 3 (#56b6c2)
           "RainbowDelimiterRed",
           "RainbowDelimiterYellow",
           "RainbowDelimiterBlue",
-          "RainbowDelimiterOrange",
           "RainbowDelimiterGreen",
-          "RainbowDelimiterViolet",
-          "RainbowDelimiterCyan",
         },
       }
+
+      -- Set the specific colors from VS Code settings
+      vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#d19a66" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterPurple", { fg = "#c678dd" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterCyan",   { fg = "#56b6c2" })
     end,
   },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,10 +1,36 @@
 return {
-  -- One Dark Pro Theme (Matches your Antigravity setup)
+  -- One Dark Pro Theme (Precision VS Code Match)
   {
     "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
+    priority = 1000,
     config = function()
       require("onedarkpro").setup({
+        colors = {
+          onedark = {
+            bg = "#16191d", -- VS Code editor.background
+            fg = "#abb2bf", -- VS Code editor.foreground
+            red = "#e06c75", -- VS Code Red
+            orange = "#d19a66", -- VS Code Orange
+            yellow = "#e5c07b", -- VS Code Yellow
+            green = "#98c379", -- VS Code Green
+            cyan = "#56b6c2", -- VS Code Cyan
+            blue = "#61afef", -- VS Code Blue
+            purple = "#c678dd", -- VS Code Purple
+            gray = "#5c6370", -- VS Code Gray
+            comment = "#7f848e", -- VS Code Comment
+          },
+        },
+        highlights = {
+          -- General UI mapping
+          CursorLine = { bg = "#2c313c" }, -- editor.lineHighlightBackground
+          LineNr = { fg = "#667187" }, -- editorLineNumber.foreground
+          CursorLineNr = { fg = "#abb2bf", bold = true }, -- editorLineNumber.activeForeground
+          Visual = { bg = "#404859" }, -- editor.selectionBackground (adjusted for visibility)
+          
+          -- Diagnostics (Error Lens style)
+          DiagnosticVirtualTextError = { italic = true },
+          DiagnosticVirtualTextWarn = { italic = true },
+        },
         options = {
           transparency = true,
           cursorline = true,
@@ -44,7 +70,7 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
-        theme = "auto", -- Automatically matches One Dark Pro
+        theme = "auto",
         component_separators = "|",
         section_separators = "",
       },
