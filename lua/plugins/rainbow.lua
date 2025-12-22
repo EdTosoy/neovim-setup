f63d44643c718661c73ce731864564954f4c13c7
edtosoy <edberto.tosoy@gmail.com>
2025-12-22 18:26:56 +0800

    feat: add rainbow brackets (rainbow-delimiters.nvim) for better code structure visibility

---
+++ lua/plugins/rainbow.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "hiphish/rainbow-delimiters.nvim",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      -- This module contains a number of default settings
+      local rainbow_delimiters = require("rainbow-delimiters")
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+          vim = rainbow_delimiters.strategy["global"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      }
+    end,
+  },
+}
