87eb9a821a408bf4fc8f1283c6c0f67a5c862bc7
edtosoy <edberto.tosoy@gmail.com>
2025-12-22 18:40:40 +0800

    feat: optimize terminal integration with leader-key shortcuts and mode escapes

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -26,12 +26,50 @@ return {
     config = function()
       require("toggleterm").setup({
         size = 20,
-        open_mapping = [[<c-\>]], -- Ctrl + backslash to toggle
+        open_mapping = [[<c-\>]],
+        hide_numbers = true,
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
         direction = "float",
+        close_on_exit = true,
+        shell = vim.o.shell,
         float_opts = {
           border = "curved",
+          winblend = 0,
+          highlights = {
+            border = "Normal",
+            background = "Normal",
+          },
         },
       })
+
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+      end
+
+      -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+      vim.api.nvim_create_autocmd("TermOpen", {
+        pattern = "term://*",
+        callback = function()
+          set_terminal_keymaps()
+        end,
+      })
+
+      -- Shortcut for floating terminal
+      vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "Toggle Floating Terminal" })
+      -- Shortcut for horizontal terminal
+      vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=15 direction=horizontal<cr>", { desc = "Toggle Horizontal Terminal" })
+      -- Shortcut for vertical terminal
+      vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", { desc = "Toggle Vertical Terminal" })
     end,
   },
 
