ffda42116fa8494b26622ed9cffa76a04e7e2e21
edtosoy <edberto.tosoy@gmail.com>
2025-12-22 18:19:03 +0800

    feat: show hidden files in neo-tree by default

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -7,7 +7,26 @@ return {
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
-    config = function()
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This makes hidden files visible by default
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            -- ".DS_Store",
+            -- "thumbs.db"
+          },
+          never_show = {
+            -- ".DS_Store",
+            -- "thumbs.db"
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("neo-tree").setup(opts)
       vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Explorer" })
     end,
   },
